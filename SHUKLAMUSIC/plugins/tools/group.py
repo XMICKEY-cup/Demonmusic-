7d82df69a6bd270d3ef6dc479f8b2f733f8479d9
wizardop10 <wizardpapahun@gmail.com>
2025-05-28 00:28:56 +0530

    Update group.py

--- SHUKLAMUSIC/plugins/tools/group.py
+++ SHUKLAMUSIC/plugins/tools/group.py
@@ -9,7 +9,7 @@ async def brah(_, msg):
 # vc off
 @app.on_message(filters.video_chat_ended)
 async def brah2(_, msg):
-       await msg.reply("**ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴇɴᴅᴇᴅ**")
+       await msg.reply("**❖ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴇɴᴅᴇᴅ ❖**")
 
 # invite members on vc
 @app.on_message(filters.video_chat_members_invited)
